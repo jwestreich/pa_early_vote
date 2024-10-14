bc2139b82b1627a511db7aa64cf316f7ddc94f25
jwestreich <jwestreich1@gmail.com>
2024-10-14 14:18:54 -0400

    vbm graph fixed, data updated through 10/14

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(lubridate)
 library(ggplot2)
 library(plotly)
+options(scipen=999)
 
 df <- read_csv("https://raw.githubusercontent.com/jwestreich/pa_early_vote/refs/heads/main/data.csv") %>%
   mutate(date = mdy(date)) %>%
@@ -19,24 +20,30 @@ df <- read_csv("https://raw.githubusercontent.com/jwestreich/pa_early_vote/refs/
          edge_d_label = paste0(edge_d * 100, "%"))
 
 df_long <- df %>%
-  pivot_longer(cols = c(split_d, split_r, split_i), names_to = "party", values_to = "value")%>%
-  mutate(party=ifelse(party=="split_d", "Democrat", party),
-         party=ifelse(party=="split_r", "Republican", party),
-         party=ifelse(party=="split_i", "Independent", party),
+  select(date, votes_d, votes_r, votes_i, split_d, split_r, split_i)%>%
+  pivot_longer(cols = c(votes_d, votes_r, votes_i, split_d, split_r, split_i),
+               names_to = c(".value", "party"),
+               names_pattern = "(votes|split)_(d|r|i)")%>%
+  mutate(party=ifelse(party=="d", "Democrat", party),
+         party=ifelse(party=="r", "Republican", party),
+         party=ifelse(party=="i", "Independent", party),
   )%>%
-  select(date, party, vbm_split=value)
+  select(date, party, split, votes)%>%
+  mutate(split=ifelse(is.na(split),.00000000000000001,split))
 
 ui <- fluidPage(
   titlePanel("Pennsylvania Early Voting Tracker"),
   mainPanel(
     tags$h3("Dem Firewall Tracker"),
+    tags$h4("How many more votes have been returned from Democrats than Republicans"),
     plotlyOutput("firewallPlot", height = "600px"),
     
     tags$h3("Dem Return Rate Edge Tracker"),
+    tags$h4("Percentage point difference between Democrat ballot return rate and Republican ballot return rate"),
     plotlyOutput("edgedPlot", height = "600px"),
     
     tags$h3("Vote By Mail Split Tracker"),
-    plotOutput("vbmSplitPlot", height = "600px"),  # New plot added
+    plotlyOutput("vbmSplitPlot", height = "600px"),
     
     tags$div(
       style = "text-align: left; margin-top: 20px;",
@@ -55,6 +62,7 @@ ui <- fluidPage(
     )
   )
 )
+
 
 server <- function(input, output) {
   output$firewallPlot <- renderPlotly({
@@ -79,7 +87,7 @@ server <- function(input, output) {
   output$edgedPlot <- renderPlotly({
     p2 <- ggplot(data = df, aes(x = date)) +
       geom_line(aes(y = edge_d), color = "orange", size = 1.5) +
-      geom_line(aes(y = edge_d, text = paste("Date:", date, "<br>D Return Edge:", edge_d_label)), 
+      geom_line(aes(y = edge_d, text = paste("Date:", date, "<br>D Return Edge:", scales::percent(edge_d, accuracy = 0.1))), 
                 color = "orange", size = 1.5) +
       scale_y_continuous(limits = c(0, 0.2), breaks = seq(0, 0.2, 0.05), labels = scales::percent) +
       labs(x = "Date", y = "D Return Rate Edge") +
@@ -93,24 +101,28 @@ server <- function(input, output) {
     ggplotly(p2, tooltip = c("text"))
   })
   
-  output$vbmSplitPlot <- renderPlot({
-    ggplot(df_long, aes(x = date, y = vbm_split, fill = party)) +
-      geom_bar(stat = "identity", position = "fill") +
+  output$vbmSplitPlot <- renderPlotly({
+    p3 <- ggplot(df_long, aes(x = date, y = split, fill = party, 
+                              text = paste0("Party: ", party, "<br>",
+                                            "Date: ", format(date, "%b %d"), "<br>",
+                                            "Vote By Mail Split: ", scales::percent(split, accuracy = 0.1), "<br>",
+                                            "Number of Votes : ", scales::comma(votes)))) +
+      geom_bar(stat = "identity", position = "stack") +
       scale_y_continuous(labels = scales::percent_format()) +
       scale_fill_manual(values = c("Republican" = "lightcoral", "Independent" = "lightgray", "Democrat" = "lightblue")) +
-      geom_text(aes(label = scales::percent(vbm_split, accuracy = 0.1)), 
-                position = position_fill(vjust = 0.5), size = 5)+
-      scale_x_date(date_labels = "%b %d", breaks = df_long$date) +
+      scale_x_date(date_labels = "%b %d") +
       labs(x = "Date", y = "Vote By Mail Split", fill = "Party") +
       theme_classic() +
-      theme(axis.text.x = element_text(color = "black", size = 16),
-            axis.text.y = element_text(color = "black", size = 16),
-            axis.title.x = element_text(size = 20),
-            axis.title.y = element_text(size = 20),
+      theme(axis.text.x = element_text(color = "black", size = 12),
+            axis.text.y = element_text(color = "black", size = 12),
+            axis.title.x = element_text(size = 14),
+            axis.title.y = element_text(size = 14),
             legend.text = element_text(size = 16), 
             legend.title = element_text(size = 18))
     
+    ggplotly(p3, tooltip = "text")
   })
+  
 }
 
 shinyApp(ui = ui, server = server)
